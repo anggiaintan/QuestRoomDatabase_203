f7f05dd206275731ea1f4a8a6037bd803de270ab
Anggia Intan <intananggia23@gmail.com>
2024-12-14 22:32:33 +0700

    Menambahkan metode update untuk memperbarui informasi mahasiswa di database

--- app/src/main/java/com/example/roomlocaldb1/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocaldb1/data/dao/MahasiswaDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.roomlocaldb1.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -23,4 +24,8 @@ interface MahasiswaDao {
     //deleteMahasiswa
     @Delete
     suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    //updateMahasiswa
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
