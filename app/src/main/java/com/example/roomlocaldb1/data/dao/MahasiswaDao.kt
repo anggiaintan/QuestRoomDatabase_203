3c67c1b5bc8b4774546e4430183a8e1905c6ab8a
Anggia Intan <intananggia23@gmail.com>
2024-12-14 22:27:19 +0700

    Menambahkan query untuk mengambil semua data mahasiswa di database yg diurutkan berdasarkan nama secara ASC

--- app/src/main/java/com/example/roomlocaldb1/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocaldb1/data/dao/MahasiswaDao.kt
@@ -2,10 +2,16 @@ package com.example.roomlocaldb1.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.roomlocaldb1.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa) //menggunakan suspend karena operasinya berat (insert, update, delete)
+
+    //getALllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
 }
