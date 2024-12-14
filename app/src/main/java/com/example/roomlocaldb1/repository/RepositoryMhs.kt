508ec60130022d3eb645d84989f6e0a052b7030b
Anggia Intan <intananggia23@gmail.com>
2024-12-14 22:47:36 +0700

    Memanggil fungsi getMhs untuk mengambil data mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/roomlocaldb1/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb1/repository/RepositoryMhs.kt
@@ -1,10 +1,16 @@
 package com.example.roomlocaldb1.repository
 
 import com.example.roomlocaldb1.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
 
     //getAllMhs
     fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    //getMhs
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+
 }
