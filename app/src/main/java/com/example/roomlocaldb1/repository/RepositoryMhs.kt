9b6841e599bbedb04f95a8ccfe4464f1da1dd435
Anggia Intan <intananggia23@gmail.com>
2024-12-14 22:36:50 +0700

    Membuat fungsi getAllMhs untuk mendapatkan semua data mahasiswa dalam bentuk flow

--- app/src/main/java/com/example/roomlocaldb1/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb1/repository/RepositoryMhs.kt
@@ -4,4 +4,7 @@ import com.example.roomlocaldb1.data.entity.Mahasiswa
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    //getAllMhs
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
