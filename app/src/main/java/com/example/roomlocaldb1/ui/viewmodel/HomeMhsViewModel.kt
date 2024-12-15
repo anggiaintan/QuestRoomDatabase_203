fce5074200dc7a839ce37f7ee2b57e142c551217
Anggia Intan <intananggia23@gmail.com>
2024-12-15 11:10:59 +0700

    Menambahkan onStart untuk memberikan status loading saat aliran baru dimulai, sebelum data mahasiswa tersedia

--- app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,9 +2,11 @@ package com.example.roomlocaldb1.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.roomlocaldb1.repository.RepositoryMhs
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
@@ -15,4 +17,8 @@ class HomeMhsViewModel (
             listMhs = it.toList(),
             isLoading = false,
         ) }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
 }
