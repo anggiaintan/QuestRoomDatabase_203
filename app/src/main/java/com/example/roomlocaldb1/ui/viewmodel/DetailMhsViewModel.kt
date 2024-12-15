854309b6a1725645f6b0fa4089f3f9aae96aa019
Anggia Intan <intananggia23@gmail.com>
2024-12-15 11:30:39 +0700

    Menambahkan onStart dengan delay(600) dan catch untuk mengatur status UI dengan memberikan feedback loading dan menangani error

--- app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/DetailMhsViewModel.kt
@@ -4,9 +4,12 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.roomlocaldb1.repository.RepositoryMhs
 import com.example.roomlocaldb1.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -22,4 +25,17 @@ class DetailMhsViewModel (
                 isLoding = false,
             )
         }
+        .onStart {
+            emit(DetailUiState(isLoding = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoding = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                    ))
+        }
+
 }
