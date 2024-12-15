0457b8dd5c8c52dd09883e260713404d6f731724
Anggia Intan <intananggia23@gmail.com>
2024-12-15 11:13:17 +0700

    Menambahkan catch untuk menangani error yang terjadi saat aliran data dijalankan

--- app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/HomeMhsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.roomlocaldb1.repository.RepositoryMhs
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
@@ -20,5 +21,14 @@ class HomeMhsViewModel (
         .onStart {
             emit(HomeUiState(isLoading = true))
             delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
         }
 }
