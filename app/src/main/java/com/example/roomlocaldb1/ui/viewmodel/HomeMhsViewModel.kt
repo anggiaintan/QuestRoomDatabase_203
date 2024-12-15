7109e618f1db7aa103685a12cfeda5b4f06a007f
Anggia Intan <intananggia23@gmail.com>
2024-12-15 11:19:12 +0700

    Menambahkan stateIn yang digunakan untuk mengonversi flow menjadi StateFlow, dan membuat data class HomeUiState untuk menyimpan status UI

--- app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.roomlocaldb1.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomlocaldb1.data.entity.Mahasiswa
 import com.example.roomlocaldb1.repository.RepositoryMhs
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
@@ -31,4 +35,16 @@ class HomeMhsViewModel (
                 )
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(isLoading = true,)
+        )
 }
+
+data class HomeUiState (
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
