f205a0d5febfc0cd00bc5c2c0f8aaed40845ba56
Anggia Intan <intananggia23@gmail.com>
2024-12-15 11:42:44 +0700

    Menambahkan stateIn untuk membuat StateFlow yang bisa dibagikan antar komponen

--- app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb1/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,14 +2,17 @@ package com.example.roomlocaldb1.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomlocaldb1.repository.RepositoryMhs
 import com.example.roomlocaldb1.ui.navigation.DestinasiDetail
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -37,5 +40,12 @@ class DetailMhsViewModel (
                     errorMessage = it.message ?: "Terjadi kesalahan",
                     ))
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
 
 }
