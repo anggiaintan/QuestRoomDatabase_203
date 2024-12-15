cff8cc1a864e22ae225fc6d728a287487d8bd169
Anggia Intan <intananggia23@gmail.com>
2024-12-15 10:48:41 +0700

    Menggunakan fungsi getAllMhs untuk mendapatkan semua data mahaiswa dalam bentuk flow

--- app/src/main/java/com/example/roomlocaldb1/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb1/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.roomlocaldb1.repository
 
 import com.example.roomlocaldb1.data.dao.MahasiswaDao
 import com.example.roomlocaldb1.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,10 @@ class LocalRepositoryMhs (
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    //getAllMhs
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
 }
